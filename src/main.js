caa3d2342b3e397594d881d4662e30af498206b2
webala <webala1001@gmail.com>
2022-01-20 12:22:12 +0300

    dark mode added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
 var toggle_button;
 var container;
+const main = document.querySelector('main');
 
 let dark = false;
+
+const toggleAnimation = () => {
+    dark = !dark;
+
+    let clone = container.cloneNode(true);
+
+    if (dark) {
+        clone.classList.remove('light');
+        clone.classList.add('dark');
+    } else {
+        clone.classList.remove('dark');
+        clone.classList.add('light');
+    }
+
+    clone.classList.add('copy')
+    document.body.appendChild(clone);
+    document.body.classList.add('stop-scrolling')
+    clone.addEventListener('animationend', () => {
+        document.body.classList.remove('stop-scrolling');
+        container.remove();
+        clone.classList.remove('copy');
+        
+        declare();
+        events();
+    })
+    
+}
 
 const declare = () => {
     toggle_button = document.querySelector('.toggle-btn');
@@ -16,8 +44,3 @@ const events = () => {
 declare();
 events();
 
-const toggleAnimation = () => {
-    dark = !dark;
-
-    
-}
